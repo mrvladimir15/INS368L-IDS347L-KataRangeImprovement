5ae31d142b34363f93111b9181b690e278b84568
Jexcm <54919774+justincaamano@users.noreply.github.com>
2022-06-28 00:13:12 -0400

    Second commit

--- main.py
+++ main.py
@@ -17,3 +17,30 @@ def getLI(interval):
     a=interval.strip()
     li=a[-1]
     return li
+class Range:
+    def __init__(self, interval):
+        self.interval = interval
+    def getEquals(self, interval2):
+        fi1 = getFI(self.interval)
+        fn1 = getFI(self.interval)
+        ln1 = getFI(self.interval)
+        li1 = getFI(self.interval)
+
+        fi2 = getFI(self.interval)
+        fn2 = getFI(self.interval)
+        ln2 = getFI(self.interval)
+        li2 = getFI(self.interval)
+    # getting first interval
+        if fi1 == "(":
+            fn1 += 1
+        elif  fi1 == "[":
+            pass
+        else:
+            return "Ivalid first interval opening symbol"  
+    # getting final interval
+        if li1 == "(":
+            ln1 -= 1
+        elif  li1 == "[":
+            pass
+        else:
+            return "Ivalid Last interval opening symbol"  
--- test_main.py
+++ test_main.py
@@ -3,6 +3,7 @@ from main import getFI
 from main import getFN
 from main import getLN
 from main import getLI
+from main import getEquals
 
 def test_getFI():
     assert getFI("(2,7)") == '('
@@ -15,3 +16,5 @@ def test_getLN():
     
 def test_getLI():
     assert getLI("(2,6]") == ']'
+def test_getEquals():
+    assert getEquals("(2,7]","[3,8)") == True 
