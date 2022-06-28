4c345762d24e278e608c0a0875bf7f79ebf48003
mrvladimir15 <73143660+mrvladimir15@users.noreply.github.com>
2022-06-28 00:28:12 -0400

    Third commit

--- main.py
+++ main.py
@@ -17,19 +17,21 @@ def getLI(interval):
     a=interval.strip()
     li=a[-1]
     return li
+
 class Range:
     def __init__(self, interval):
         self.interval = interval
+    
     def getEquals(self, interval2):
         fi1 = getFI(self.interval)
-        fn1 = getFI(self.interval)
-        ln1 = getFI(self.interval)
-        li1 = getFI(self.interval)
+        fn1 = getFN(self.interval)
+        ln1 = getLN(self.interval)
+        li1 = getLI(self.interval)
 
         fi2 = getFI(self.interval)
-        fn2 = getFI(self.interval)
-        ln2 = getFI(self.interval)
-        li2 = getFI(self.interval)
+        fn2 = getFN(self.interval)
+        ln2 = getLN(self.interval)
+        li2 = getLI(self.interval)
     # getting first interval
         if fi1 == "(":
             fn1 += 1
@@ -38,9 +40,36 @@ class Range:
         else:
             return "Ivalid first interval opening symbol"  
     # getting final interval
-        if li1 == "(":
+        if li1 == ")":
             ln1 -= 1
-        elif  li1 == "[":
+        elif  li1 == "]":
             pass
         else:
             return "Ivalid Last interval opening symbol"  
+    # getting second part of interval
+        if fi2 == "(":
+            fn2 += 1
+        elif fi2 == "[":
+            pass
+        else:
+            return "Invalid second interval opening symbol"
+    # getting last part of interval
+        if li2 == ")":
+            ln2 -= 1
+        elif li2 == "]":
+            pass
+        else:
+            return "Invalid second interval closing symbol"
+        
+        aR = range(fn1, ln1)
+        bR = range(fn2, ln2)
+        
+        if aR == bR:
+            return True
+        else:
+            return False
+        
+a=Range("(2,7]")
+b=Range("[3,8)")
+
+print(a.getEquals(b.interval))
--- test_main.py
+++ test_main.py
@@ -3,18 +3,22 @@ from main import getFI
 from main import getFN
 from main import getLN
 from main import getLI
-from main import getEquals
+from main import Range
 
-def test_getFI():
-    assert getFI("(2,7)") == '('
+# def test_getFI():
+#     assert getFI("(2,7)") == '('
     
-def test_getFN():
-    assert getFN("(2,7)") == 2
+# def test_getFN():
+#     assert getFN("(2,7)") == 2
     
-def test_getLN():
-    assert getLN("(2,7)") == 7
+# def test_getLN():
+#     assert getLN("(2,7)") == 7
     
-def test_getLI():
-    assert getLI("(2,6]") == ']'
-def test_getEquals():
-    assert getEquals("(2,7]","[3,8)") == True 
+# def test_getLI():
+#     assert getLI("(2,6]") == ']'
+
+class testRange():
+    interval="(2,7]"
+    
+    def test_getEquals(self):
+        assert Range.getEquals(self,"[3,8)") == True
