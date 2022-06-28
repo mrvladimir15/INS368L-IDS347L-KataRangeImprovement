5c459c47c4db807a8f69e6bff32bc416b3e5ad69
Jexcm <54919774+justincaamano@users.noreply.github.com>
2022-06-28 16:03:05 -0400

    Setup fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="INS368-IDS345--KataRangePackage",
     version="0.1",
     author="Vladimir González, Justin Caamaño",
-    author_email="1098139@est.intec.edu.do",
+    author_email="1098139@est.intec.edu.do, 1088820@est.intec.edu.do",
     description="Kata range pair programming package, made by Vladimir González and Justin Caamaño",
     packages=setuptools.find_packages(),
     classifiers=[
